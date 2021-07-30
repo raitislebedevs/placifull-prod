982535bf555fc70e679fc41e196d7c40391420bd
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-07-30 18:50:04 +0300

    Languages which are added

--- components/layouts/Navbar/LanguageSelect/languages.js
+++ components/layouts/Navbar/LanguageSelect/languages.js
@@ -6,20 +6,20 @@ const languages = [
     },
     label: 'English',
   },
-  {
-    value: {
-      lang: 'lv',
-      flag: 'LV',
-    },
-    label: 'Latviešu',
-  },
-  {
-    value: {
-      lang: 'ee',
-      flag: 'EE',
-    },
-    label: 'Estonian',
-  },
+  // {
+  //   value: {
+  //     lang: 'lv',
+  //     flag: 'LV',
+  //   },
+  //   label: 'Latviešu',
+  // },
+  // {
+  //   value: {
+  //     lang: 'ee',
+  //     flag: 'EE',
+  //   },
+  //   label: 'Estonian',
+  // },
   {
     value: {
       lang: 'lt',
@@ -27,20 +27,20 @@ const languages = [
     },
     label: 'Lithuanian',
   },
-  {
-    value: {
-      lang: 'de',
-      flag: 'DE',
-    },
-    label: 'Deutsche',
-  },
-  {
-    value: {
-      lang: 'ru',
-      flag: 'RU',
-    },
-    label: 'Pу́сский',
-  },
+  // {
+  //   value: {
+  //     lang: 'de',
+  //     flag: 'DE',
+  //   },
+  //   label: 'Deutsche',
+  // },
+  // {
+  //   value: {
+  //     lang: 'ru',
+  //     flag: 'RU',
+  //   },
+  //   label: 'Pу́сский',
+  // },
   {
     value: {
       lang: 'cn',
@@ -56,27 +56,27 @@ const languages = [
     label: 'Português',
   },
 
-  {
-    value: {
-      lang: 'in',
-      flag: 'IN',
-    },
-    label: 'हिन्दी, हिंदी',
-  },
   // {
   //   value: {
-  //     lang: 'jp',
-  //     flag: 'JP',
+  //     lang: 'in',
+  //     flag: 'IN',
   //   },
-  //   label: '日本人',
+  //   label: 'हिन्दी, हिंदी',
   // },
   {
     value: {
-      lang: 'id',
-      flag: 'ID',
+      lang: 'jp',
+      flag: 'JP',
     },
-    label: 'Bahasa Indonesia',
+    label: '日本人',
   },
+  // {
+  //   value: {
+  //     lang: 'id',
+  //     flag: 'ID',
+  //   },
+  //   label: 'Bahasa Indonesia',
+  // },
   {
     value: {
       lang: 'es',
