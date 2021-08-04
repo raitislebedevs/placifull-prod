c0b9416d8a905474cd12d689ac0bddc9d19fe35d
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-08-04 09:54:18 +0300

    Squashed commit of the following:
    commit 9bd28f33df2b8b9f05024b5778247cfc8b9ebe06
    Author: raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
    Date:   Wed Aug 4 09:53:56 2021 +0300
    
        Update google Analytics
    
    commit a292a50df18329d72f9d6cfb7b2f0f64ebb4aa34
    Author: raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
    Date:   Wed Aug 4 08:15:35 2021 +0300
    
        Update RegisterForm.js

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
             rel="stylesheet"
           />
+          <meta
+            name="google-site-verification"
+            content="seGi1OVhs3Cq9XBTOdTRoPTsRn2qWuYEyKdw9PT7I9k"
+          />
           <link rel="icon" href={logo} sizes="any" type="image/svg+xml" />
         </Head>
         <body>
