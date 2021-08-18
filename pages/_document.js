ced02b903850583e9a412febd09cee5e86aec95c
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-08-18 11:07:34 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -27,12 +27,6 @@ class MyDocument extends Document {
           `,
             }}
           />
-          {/* <!-- Go to www.addthis.com/dashboard to customize your tools --> */}
-          <script
-            type="text/javascript"
-            src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-611cb38658523fb3"
-          ></script>
-
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
