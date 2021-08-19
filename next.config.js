06053ad9bb5765d4974afa4ff9ac273a3e58a75b
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-08-19 14:45:32 +0300

    Next 11 version

--- next.config.js
+++ next.config.js
@@ -10,9 +10,7 @@ module.exports = withImages({
     EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
     EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
   },
-  future: {
-    webpack5: true,
-  },
+
   images: {
     domains: ['placifull-static.s3.eu-central-1.amazonaws.com'],
   },
