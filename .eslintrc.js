4777e41db52bfaaba74fb8b4b589bc44c1ed59b4
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-08-19 14:53:01 +0300

    Test Two

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,51 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2020': true,
-    'node': true,
-    'jest': true
+  env: {
+    browser: true,
+    es2020: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
   },
-  'extends': [
+  root: true,
+  parser: 'babel-eslint',
+  extends: [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
   ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 11,
-    'sourceType': 'module'
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  'plugins': [
-    'react'
-  ],
-  'rules': {
-    'indent': [
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    indent: [
       'error',
       2,
       {
-        'SwitchCase': 1
-      }
+        SwitchCase: 1,
+      },
     ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi-style': [
-      'error',
-      'last'
-    ],
+    quotes: ['error', 'single'],
+    'semi-style': ['error', 'last'],
     'no-var': 1,
     'react/prop-types': 0,
     'no-unused-vars': 0,
     'no-prototype-builtins': 0,
     'react/display-name': 0,
-    'eqeqeq': 1,
-    'react/react-in-jsx-scope': 0
-  }
-}
+    'react/react-in-jsx-scope': 0,
+  },
+};
