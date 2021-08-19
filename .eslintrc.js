23122f8d330c2e9abc5996b6d383f6bc72372104
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-08-19 15:00:42 +0300

    Revert "Test Two"
    This reverts commit 4777e41db52bfaaba74fb8b4b589bc44c1ed59b4.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,46 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-    jest: true,
+  'env': {
+    'browser': true,
+    'es2020': true,
+    'node': true,
+    'jest': true
   },
-  root: true,
-  parser: 'babel-eslint',
-  extends: [
+  'extends': [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react-hooks/recommended',
-    'airbnb',
+    'plugin:react/recommended'
   ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    'ecmaVersion': 11,
+    'sourceType': 'module'
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  plugins: ['react'],
-  rules: {
-    indent: [
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
       'error',
       2,
       {
-        SwitchCase: 1,
-      },
+        'SwitchCase': 1
+      }
     ],
-    quotes: ['error', 'single'],
-    'semi-style': ['error', 'last'],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi-style': [
+      'error',
+      'last'
+    ],
     'no-var': 1,
     'react/prop-types': 0,
     'no-unused-vars': 0,
     'no-prototype-builtins': 0,
     'react/display-name': 0,
-    'react/react-in-jsx-scope': 0,
-  },
-};
+    'eqeqeq': 1,
+    'react/react-in-jsx-scope': 0
+  }
+}
