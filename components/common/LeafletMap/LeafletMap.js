e4f0fad0875c97b9b35b50fb41bdf7a2675445ad
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-08-30 15:52:02 +0300

    direct input

--- components/common/LeafletMap/LeafletMap.js
+++ components/common/LeafletMap/LeafletMap.js
@@ -2,7 +2,7 @@ import 'leaflet/dist/leaflet.css';
 import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css';
 import 'leaflet/dist/leaflet.js';
 import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder.js';
-import 'leaflet-draw/dist/leaflet.draw.css';
+//import 'leaflet-draw/dist/leaflet.draw.css';
 
 import React, { useState, useEffect } from 'react';
 import L from 'leaflet';
