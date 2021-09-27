d866f0f8c60438db9bd083252dfd52a8f0671af2
raitislebedevs <54849701+raitislebedevs@users.noreply.github.com>
2021-09-27 11:55:01 +0300

    Transport Filter Added

--- components/common/TransportPost/TransportPost.js
+++ components/common/TransportPost/TransportPost.js
@@ -164,9 +164,13 @@ const TransportSubmit = (props) => {
         getSubscriptions(user.id);
       }
 
-      setTimeout(() => {
-        router.push(`/transport/${listingId}`);
-      }, 1500);
+      TransportListingService.NOTIFY_USERS({
+        id: listingId,
+      });
+
+      // setTimeout(() => {
+      //   router.push(`/transport/${listingId}`);
+      // }, 1500);
     } catch (e) {
       TostifyCustomContainer(
         'error',
@@ -290,11 +294,6 @@ const TransportSubmit = (props) => {
         setsubmitCurrency={setsubmitCurrency}
         t={t}
       />
-      <Gallery
-        inputValues={inputValues}
-        handleOnChange={handleOnChange}
-        t={t}
-      />
       <DetailInformation
         inputValues={inputValues}
         addressPosition={addressPosition}
@@ -305,6 +304,11 @@ const TransportSubmit = (props) => {
         submitCurrency={submitCurrency}
         tagOptions={tagOptions}
         setTagOptions={setTagOptions}
+        t={t}
+      />
+      <Gallery
+        inputValues={inputValues}
+        handleOnChange={handleOnChange}
         t={t}
       />
       <SocialLinks
