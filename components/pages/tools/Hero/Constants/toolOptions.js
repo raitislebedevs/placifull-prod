67fcc0b0dba8b5f42611e18dd8cf1dd93194ae67
raitis.lebedevs1 <raitis.lebedevs@gmail.com>
2022-12-26 10:53:35 +0200

    Revert "Update toolOptions.js"
    This reverts commit 3f3851febc28bebd610ee65b52116687440053c7.

--- components/pages/tools/Hero/Constants/toolOptions.js
+++ components/pages/tools/Hero/Constants/toolOptions.js
@@ -1,17 +1,17 @@
-export const toolOptions = t => [
-  // {
-  //   key: 0,
-  //   title: t('tools:tool-types.all'),
-  //   filter: '',
-  // },
+export const toolOptions = (t) => [
+  {
+    key: 0,
+    title: t('tools:tool-types.all'),
+    filter: '',
+  },
   {
     key: 1,
     title: t('tools:tool-types.mortgage'),
-    filter: 'mortgage'
-  }
-  // {
-  //   key: 2,
-  //   title: t('tools:tool-types.salary'),
-  //   filter: 'salary',
-  // },
+    filter: 'mortgage',
+  },
+  {
+    key: 2,
+    title: t('tools:tool-types.salary'),
+    filter: 'salary',
+  },
 ];
